3897ca495f7ecfbea75ae7508fdc7568f6f93c78
adhulip <rudysback@gmail.com>
2015-05-07 11:22:21 -0500

    Anirudh | #3 | push failing test to check snap is working

--- spec/controllers/dashtag_page_controller_spec.rb
+++ spec/controllers/dashtag_page_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe DashtagPageController do
 	context 'sample test' do 
 		it 'does know how to add 1 and 1' do 
-			expect(1 + 1).to eq 2
+			expect(1 + 3).to eq 2
 		end
 	end
 end
