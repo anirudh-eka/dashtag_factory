874054f337e7df1eeed6baa217019e459e69d887
Danielle Erickson <derickso@thoughtworks.com>
2015-05-13 14:34:36 -0500

    Danielle | #11 | Added fields for other env variables to launch_page, added test for env variables in heroku app service spec

--- app/services/heroku_app_service.rb
+++ app/services/heroku_app_service.rb
@@ -11,13 +11,31 @@ class HerokuAppService
             "env" => {
                 "HASHTAGS" => app_options[:hashtags],
                 "TWITTER_CONSUMER_KEY" => app_options[:twitter_consumer_key],
-                "TWITTER_CONSUMER_SECRET" => app_options[:twitter_consumer_secret]
-	            }
+                "TWITTER_CONSUMER_SECRET" => app_options[:twitter_consumer_secret],
+                
+                "CENSORED_WORDS" => app_options[:censored_words],
+                "CENSORED_USERS" => app_options[:censored_users],
+                "INSTAGRAM_CLIENT_ID" => app_options[:instagram_client_id],
+                "TWITTER_USERS" => app_options[:twitter_users],
+                "INSTAGRAM_USERS" => app_options[:instagram_users],
+                "INSTAGRAM_USER_IDS" => app_options[:instagram_user_ids],
+                "FONT_FAMILY" => app_options[:font_family],
+                "HEADER_TITLE" => app_options[:header_title],
+                "HEADER_LINK" => app_options[:header_link],
+                "HEADER_COLOR" => app_options[:header_color],
+                "BACKGROUND_COLOR" => app_options[:background_color],
+                "POST_COLOR_1" => app_options[:post_color_1],
+                "POST_COLOR_2" => app_options[:post_color_2],
+                "POST_COLOR_3" => app_options[:post_color_3],
+                "POST_COLOR_4" => app_options[:post_color_4],
+                "API_RATE" => app_options[:api_rate]
+                }
 	        }
 	    }
     	
     	begin
-			client.app_setup.create(arguments) 
+            client.app_setup.create(arguments) 
+            return "Deploy Successful!"
 		rescue Exception => e
             return JSON.load(e.response.data[:body])["message"]
 		end 
--- spec/services/heroku_app_service_spec.rb
+++ spec/services/heroku_app_service_spec.rb
@@ -12,7 +12,51 @@ describe HerokuAppService do
 			expect(app_setup_mock.create_args["app"]["name"]).to eq(app_options[:name])
 		end
 
-		context "When app_setup creation fails" do
+		it 'sets the env variables correctly' do 
+			app_setup_mock = AppSetupMock.new
+			client_mock = double("client", app_setup: app_setup_mock)
+			
+			app_options = {hashtags: "hash", 
+							twitter_consumer_key: "key", 
+							twitter_consumer_secret: "secret",
+							censored_words: "bad",
+							censored_users: "bad user",
+							instagram_client_id: "insta client id",
+							twitter_users: "monkeygirltwitta",
+							instagram_users: "monkeygirlinsta",
+							instagram_user_ids: "insta id",
+							font_family: "comic sans",
+							header_title: "Cute Monkeys",
+							header_link: "linktomonkeys",
+							header_color: "purple",
+							background_color: "blue",
+							post_color_1: "orange",
+							post_color_2: "red",
+							post_color_3: "maroon",
+							post_color_4: "violet"}			
+
+			HerokuAppService.create_app(client_mock, app_options)
+			expect(app_setup_mock.create_args["overrides"]["env"]["HASHTAGS"]).to eq(app_options[:hashtags])
+			expect(app_setup_mock.create_args["overrides"]["env"]["TWITTER_CONSUMER_KEY"]).to eq(app_options[:twitter_consumer_key])
+			expect(app_setup_mock.create_args["overrides"]["env"]["TWITTER_CONSUMER_SECRET"]).to eq(app_options[:twitter_consumer_secret])
+			expect(app_setup_mock.create_args["overrides"]["env"]["CENSORED_WORDS"]).to eq(app_options[:censored_words])
+			expect(app_setup_mock.create_args["overrides"]["env"]["CENSORED_USERS"]).to eq(app_options[:censored_users])
+			expect(app_setup_mock.create_args["overrides"]["env"]["INSTAGRAM_CLIENT_ID"]).to eq(app_options[:instagram_client_id])
+			expect(app_setup_mock.create_args["overrides"]["env"]["TWITTER_USERS"]).to eq(app_options[:twitter_users])
+			expect(app_setup_mock.create_args["overrides"]["env"]["INSTAGRAM_USERS"]).to eq(app_options[:instagram_users])
+			expect(app_setup_mock.create_args["overrides"]["env"]["INSTAGRAM_USER_IDS"]).to eq(app_options[:instagram_user_ids])
+			expect(app_setup_mock.create_args["overrides"]["env"]["FONT_FAMILY"]).to eq(app_options[:font_family])
+			expect(app_setup_mock.create_args["overrides"]["env"]["HEADER_TITLE"]).to eq(app_options[:header_title])
+			expect(app_setup_mock.create_args["overrides"]["env"]["HEADER_LINK"]).to eq(app_options[:header_link])
+			expect(app_setup_mock.create_args["overrides"]["env"]["HEADER_COLOR"]).to eq(app_options[:header_color])
+			expect(app_setup_mock.create_args["overrides"]["env"]["BACKGROUND_COLOR"]).to eq(app_options[:background_color])
+			expect(app_setup_mock.create_args["overrides"]["env"]["POST_COLOR_1"]).to eq(app_options[:post_color_1])
+			expect(app_setup_mock.create_args["overrides"]["env"]["POST_COLOR_2"]).to eq(app_options[:post_color_2])
+			expect(app_setup_mock.create_args["overrides"]["env"]["POST_COLOR_3"]).to eq(app_options[:post_color_3])
+			expect(app_setup_mock.create_args["overrides"]["env"]["POST_COLOR_4"]).to eq(app_options[:post_color_4])
+		end
+
+		context "When creation fails" do
 			it "should return error message" do
 				app_setup_mock = double("app_setup")
 				mock_response = double("response")
