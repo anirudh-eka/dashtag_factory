40d029636140108497d1c2968466728337754cd4
adhulip <rudysback@gmail.com>
2015-05-26 11:50:30 -0500

    Danielle & Anirudh | #16 | if fails during app setup creation include heroku's error message, if fails during polling set failure message to 'Heroku deploy failed.' (and don't set any message with success)

--- app/services/app_service.rb
+++ app/services/app_service.rb
@@ -4,12 +4,14 @@ class AppService
 	end
 	
 	def create(client, request_body)
-    begin
-		  app_setup = @app_setup_service.create(client, request_body)
-    rescue AppSetupException => e
-      return {"status" => "failed", "message" => e.message}
-    end
-    app_setup_status_info = app_setup.poll_until_app_status_resolved
-    {"status" => app_setup_status_info["status"]}
+        begin
+    		  app_setup = @app_setup_service.create(client, request_body)
+        rescue AppSetupException => e
+          return {"status" => "failed", "message" => e.message}
+        end
+        app_setup_status_info = app_setup.poll_until_app_status_resolved
+        
+        return {"status" => "succeeded" } if app_setup_status_info["status"] == "succeeded"
+        {"status" => "failed", "message" => "Heroku deploy failed."} if app_setup_status_info["status"] == "failed"
 	end
 end
--- spec/services/app_service_spec.rb
+++ spec/services/app_service_spec.rb
@@ -43,17 +43,17 @@ describe AppService do
       end
 
       context "when polling status results in a failed" do 
-        xit "should return hash with status failed and error message" do 
-          failed_message = {"id"=>"c3cd21ed-44b8-4cb9-8d56-c0781cee637c", 
+        it "should return hash with status failed and error message" do 
+          response_failure = {"id"=>"c3cd21ed-44b8-4cb9-8d56-c0781cee637c", 
             "failure_message"=>"invalid app.json", 
             "status"=>"failed", 
             "app"=>{"id"=>"3be4c77d-647a-46c5-bfe5-072e834feba0", "name"=>"newpagedashtag33"}, 
             "build"=>{"id"=>nil, "status"=>nil}, 
             "manifest_errors"=>["config var \"HASHTAGS\" is required", "config var \"CENSORED_WORDS\" is required", "config var \"CENSORED_USERS\" is required", "config var \"TWITTER_CONSUMER_KEY\" is required", "config var \"TWITTER_CONSUMER_SECRET\" is required", "config var \"INSTAGRAM_CLIENT_ID\" is required", "config var \"TWITTER_USERS\" is required", "config var \"INSTAGRAM_USERS\" is required", "config var \"INSTAGRAM_USER_IDS\" is required", "config var \"FONT_FAMILY\" is required", "config var \"HEADER_TITLE\" is required", "config var \"HEADER_LINK\" is required", "config var \"HEADER_COLOR\" is required", "config var \"BACKGROUND_COLOR\" is required", "config var \"POST_COLOR_1\" is required", "config var \"POST_COLOR_2\" is required", "config var \"POST_COLOR_3\" is required", "config var \"POST_COLOR_4\" is required", "config var \"API_RATE\" is required"], "postdeploy"=>{"output"=>nil, "exit_code"=>nil}, "resolved_success_url"=>nil, "created_at"=>"2015-05-22T21:45:08+00:00", "updated_at"=>"2015-05-22T21:45:09+00:00"}
-          allow(heroku_app_setup_mock).to receive(:info).with(app_setup_id).and_return({"status" => "failed"})
+          allow(heroku_app_setup_mock).to receive(:info).with(app_setup_id).and_return(response_failure)
 
           result = app_service.create(client_mock, request_body)
-          expect(result).to eq({"status"=> "failed"})
+          expect(result).to eq({"status"=> "failed", "message" => "Heroku deploy failed."})
         end 
       end
     end
